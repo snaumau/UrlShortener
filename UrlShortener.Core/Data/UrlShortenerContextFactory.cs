14a5cb221156bcd938b07241549581ab7599b84a
Siarhei Naumau <s.naumau@gmail.com>
2022-11-01 15:19:12 +0300

    feat: add context factory

---
+++ UrlShortener.Core/Data/UrlShortenerContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+namespace UrlShortener.Core.Data
+{
+    public class UrlShortenerContextFactory : IDesignTimeDbContextFactory<UrlShortenerContext>
+    {
+        public UrlShortenerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UrlShortenerContext>();
+            optionsBuilder.UseMySql(
+                @"Server=localhost;Port=3307;Database=UrlShortener;Uid=root;Pwd=04081202;",
+                ServerVersion.Create(
+                    new Version(10, 3, 36),
+                    ServerType.MariaDb),
+                options => options.EnableRetryOnFailure());
+
+            return new UrlShortenerContext(optionsBuilder.Options);
+        }
+    }
+}
