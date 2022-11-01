bf18282ba1e881e18a98d84ef8606dc230988a12
Siarhei Naumau <s.naumau@gmail.com>
2022-11-01 14:48:28 +0300

    feat: add date time field in the model

--- UrlShortener.Models/Entities/Url.cs
+++ UrlShortener.Models/Entities/Url.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string UrlLong { get; set; } = string.Empty;
         public string UrlShort { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
         public int Counter { get; set; }
     }
 }
