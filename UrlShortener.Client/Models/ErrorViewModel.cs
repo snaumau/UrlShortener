21bad4ab7557974badc02770ffc92b7c236de9c9
Siarhei Naumau <s.naumau@gmail.com>
2022-11-01 15:38:58 +0300

    feat: initial the web client project

---
+++ UrlShortener.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrlShortener.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
