2330f941394c8af811b8bae03b95745705c943a5
Siarhei Naumau <s.naumau@gmail.com>
2022-11-01 21:52:08 +0300

    feat: add home controller

---
+++ UrlShortener.Client/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Client.Models;
+using UrlShortener.Models.Interfaces;
+
+namespace UrlShortener.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUrlRepository _urlRepository;
+
+        public HomeController(IUrlRepository urlRepository)
+        {
+            _urlRepository = urlRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                Urls = _urlRepository.GetAllUrl
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
