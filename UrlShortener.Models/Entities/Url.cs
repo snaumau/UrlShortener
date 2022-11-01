a27f56db3c284887037c9dc77ab1b66ebcec2d5f
Siarhei Naumau <s.naumau@gmail.com>
2022-11-01 14:21:10 +0300

    feat: add url model

---
+++ UrlShortener.Models/Entities/Url.cs
@@ -0,0 +1,10 @@
+﻿namespace UrlShortener.Models.Entities
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public string UrlLong { get; set; } = string.Empty;
+        public string UrlShort { get; set; } = string.Empty;
+        public int Counter { get; set; }
+    }
+}
