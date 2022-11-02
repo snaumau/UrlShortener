f6857db55d38613cd90832e9c868f867db0967d3
Siarhei Naumau <s.naumau@gmail.com>
2022-11-02 19:35:34 +0300

    feat: add shortener service for admin controller

---
+++ UrlShortener.Client/Services/UrlShortenerService.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace UrlShortener.Client.Services
+{
+    public static class UrlShortenerService
+    {
+        private static readonly string _alphabet = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_";
+        private static readonly int _base = _alphabet.Length;
+
+        public static string Encode(string url)
+        {
+            int num = GetHash(url);
+            var sb = new StringBuilder();
+            while (num > 0)
+            {
+                sb.Insert(0, _alphabet.ElementAt(num % _base));
+                num = num / _base;
+            }
+
+            string shortUrl = $"https://shortener.com/{sb.ToString()}";
+            return shortUrl;
+        }
+
+        private static int GetHash(string str)
+        {
+            var num = 0;
+            for (var i = 0; i < str.Length; i++)
+            {
+                num = num * _base + _alphabet.IndexOf(str.ElementAt(i));
+            }
+            return Math.Abs(num);
+        }
+    }
+}
