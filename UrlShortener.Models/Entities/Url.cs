cedf822a4e73f211067d3468228e2e7ddabbb98b
Siarhei Naumau <s.naumau@gmail.com>
2022-11-03 11:51:12 +0300

    feat: add validation in the url model

--- UrlShortener.Models/Entities/Url.cs
+++ UrlShortener.Models/Entities/Url.cs
@@ -1,11 +1,27 @@
-﻿namespace UrlShortener.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Models.Entities
 {
     public class Url
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter you url")]
+        [DataType(DataType.Url)]
+        [Display(Name = "Your url")]
         public string UrlLong { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter short url")]
+        [DataType(DataType.Url)]
+        [Display(Name = "Short url")]
         public string UrlShort { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter data")]
+        [DataType(DataType.Time)]
         public DateTime CreatedAt { get; set; }
+
+        [Required(ErrorMessage = "Please enter count")]
+        [Display(Name = "Please enter number of counter")]
         public int Counter { get; set; }
     }
 }
