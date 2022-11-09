e77a579ed405d38b524f298352ae7f0430335c7e
Siarhei Naumau <s.naumau@gmail.com>
2022-11-10 00:33:52 +0300

    feat: add redirect action

--- UrlShortener.Client/Controllers/HomeController.cs
+++ UrlShortener.Client/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 using UrlShortener.Client.DTOs;
 using UrlShortener.Client.ViewModels;
 
@@ -7,7 +8,7 @@ namespace UrlShortener.Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string _baseUrl = "https://localhost:7234/api/url/";
+        private readonly string _baseUrl = "https://localhost:7234/api/Url";
 
         [HttpGet]
         public async Task<IActionResult> Index()
@@ -26,19 +27,35 @@ namespace UrlShortener.Client.Controllers
             return View();
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Index(int id)
+        [HttpGet("{urlId}")]
+        public async Task<IActionResult> Index(string urlId, UrlDto urlDto)
         {
             // Get model
             var client = new HttpClient();
-            var response = await client.GetAsync($"{_baseUrl}/{id}");
+            var response = await client.GetAsync($"{_baseUrl}/GetByString?urlId={urlId}");
+            
             if (response.IsSuccessStatusCode)
+                urlDto = JsonConvert.DeserializeObject<UrlDto>(await response.Content.ReadAsStringAsync());
+
+            if (urlDto is null)
+                return NotFound();
+
+            // Save original link
+            //Uri redirectLink = urlDto.UrlLong;
+
+            // Increment counter
+            urlDto.Counter++;
+            string json = JsonConvert.SerializeObject(urlDto);
+            var putResponse = await client.PutAsync($"{_baseUrl}/?id={urlDto.Id}",
+                new StringContent(json, Encoding.UTF8, "application/json"));
+
+            if (!putResponse.IsSuccessStatusCode)
             {
-                var url = JsonConvert.DeserializeObject<UrlDto>(await response.Content.ReadAsStringAsync());
-                var responseUri = $"{client.BaseAddress}";
+                return BadRequest();
             }
 
-            return Redirect("");
+            // Redirect to original link
+            return Redirect(urlDto.UrlLong);
         }
     }
 }
