f3c63331b5d82c133b2e4877ed39e3399f369a24
Siarhei Naumau <s.naumau@gmail.com>
2022-11-09 12:02:52 +0300

    feat: add dto models

---
+++ UrlShortener.API/DTOs/EditUrlDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UrlShortener.API.DTOs
+{
+    public class EditUrlDto
+    {
+        public string UrlLong { get; set; } = string.Empty;
+        public string UrlShort { get; set; } = string.Empty;
+        public int Counter { get; set; }
+    }
+}
