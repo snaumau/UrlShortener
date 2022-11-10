ee74cc5ab2a7f70cdd25abeaad97afa9e2bfd337
Siarhei Naumau <s.naumau@gmail.com>
2022-11-10 11:40:25 +0300

    feat: add validation for the client side

--- UrlShortener.Client/DTOs/UrlDto.cs
+++ UrlShortener.Client/DTOs/UrlDto.cs
@@ -8,23 +8,18 @@ namespace UrlShortener.Client.DTOs
 
         [Required(ErrorMessage = "Please enter you url")]
         [DataType(DataType.Url)]
-        [Display(Name = "Your url")]
         public string UrlLong { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter short url")]
-        [DataType(DataType.Url)]
-        [Display(Name = "Short url")]
         public string UrlShort { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Please enter data")]
+        [Required]
         [DataType(DataType.Time)]
-        [Display(Name = "Date")]
         public DateTime CreatedAt { get; set; }
 
         [Required(ErrorMessage = "Please enter count")]
         [Range(1, int.MaxValue)]
         [DataType(DataType.Currency)]
-        [Display(Name = "Please enter number of counter")]
         public int Counter { get; set; }
     }
 }
